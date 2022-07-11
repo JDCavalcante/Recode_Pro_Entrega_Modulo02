1fa28cd68b68a3228730938dec71b022eb16de6d
JDCavalcante <cavalcantefilho.jd@gmail.com>
2022-07-11 18:41:02 -0300

    Commit teste

---
+++ JS/calculadora.js
@@ -0,0 +1,29 @@
+
+
+function soma(){
+    let a = Number(document.getElementById("entrada1").value)
+    let b = Number(document.getElementById("entrada2").value)
+    let soma = a + b
+    document.getElementById("result").value = soma
+}
+
+function subtrair(){
+  
+
+    let soma = a - b
+
+    alert("A subtração dos valores é: " + soma)
+}
+function dividir(){
+ 
+
+    let soma = a / b
+
+    alert("A divisão dos valores é: " + soma)
+}
+function multiplicar(){
+
+    let soma = a * b
+
+    alert("A multiplicação dos valores é: " + soma)
+}
